08fe6c9261cabc320d8f14231ba9cb9d63cc0483
Sven Jatzlau <Sven.Jatzlau@fau.de>
2019-07-29 14:30:31 +0200

    initial version of eliza

--- src/views/game.js
+++ src/views/game.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Terminal from 'terminal-in-react'
-//import Rivescript from 'Rivescript'
+import RiveScript from 'rivescript'
 
 const showMsg = () => 'Hello World'
 
@@ -26,6 +26,7 @@ function Game() {
           popup: () => alert('Terminal in React'),
           chat: () => startchatbot()
         }}
+        watchConsoleLogging
         actionHandlers={{
           handleClose: toggleClose => {
             // do nothing
@@ -50,7 +51,48 @@ function Game() {
 }
 
 function startchatbot() {
-  alert('Chatbot has been started!')
+  var bot = new RiveScript();
+
+// Load a directory full of RiveScript documents (.rive files). This is for
+// Node.JS only: it doesn't work on the web!
+//bot.loadDirectory("brain").then(loading_done).catch(loading_error);
+
+// Load an individual file.
+bot.loadFile("../brain/eliza.rive").then(loading_done).catch(loading_error);
+
+// Load a list of files all at once (the best alternative to loadDirectory
+// for the web!)
+//bot.loadFile([
+//  "brain/begin.rive",
+//  "brain/admin.rive",
+//  "brain/clients.rive"
+//]).then(loading_done).catch(loading_error);
+
+// All file loading operations are asynchronous, so you need handlers
+// to catch when they've finished. If you use loadDirectory (or loadFile
+// with multiple file names), the success function is called only when ALL
+// the files have finished loading.
+function loading_done() {
+  console.log("Bot has finished loading!");
+  // Now the replies must be sorted!
+  bot.sortReplies();
+
+  // And now we're free to get a reply from the brain!
+
+  // RiveScript remembers user data by their username and can tell
+  // multiple users apart.
+  let username = "local-user";
+
+  // NOTE: the API has changed in v2.0.0 and returns a Promise now.
+  bot.reply(username, "sorry!").then(function(reply) {
+    console.log("The bot says: " + reply);
+  });
+}
+
+// It's good to catch errors too!
+function loading_error(error, filename, lineno) {
+  console.log("Error when loading files: " + error);
+}
 }
 
 export default Game
